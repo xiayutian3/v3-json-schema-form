c403f0124235ae9eb03f5dc5bd6120f3adfdfe42
1127071993@qq.com <aini2009626>
2022-01-20 22:00:48 +0800

    2-6 setup的运用和其意义

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" :age="18"/>
+  <p>{{name}}</p>
+  <p>{{computedRef}}</p>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent, reactive, ref, watchEffect } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
+  },
+  setup (props, { slots, attrs, emit }) {
+    const state = reactive({
+      name: 'heelo'
+    })
+    // setTimeout(() => {
+    //   state.name += '1'
+    // }, 1000)
+
+    // ref {value:xxx}
+    const name = ref('heelo')
+
+    // computed
+
+    const computedRef = computed(() => {
+      return name.value + '2'
+    })
+    // watchEffect
+    watchEffect(() => {
+      console.log('watch', name.value)
+    })
+    return {
+      name,
+      computedRef
+    }
   }
 })
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,26 @@
-import { createApp } from 'vue'
+
+// h函数里面最终还是调用createVNode
+import { createApp, defineComponent, h, createVNode } from 'vue'
 import App from './App.vue'
+
+// import HelloWorld from './components/HelloWorld.vue'
+
+// // 引入图片模块（在template中vue-loder先出里，在配和file-loader或者url-loader，生成真正的图片地址）
+// const img = require('./assets/logo.png')
+// // render 函数
+// const App = defineComponent({
+//   render () {
+//     return createVNode('div', { id: 'app' }, [
+//       createVNode('img', {
+//         alt: 'Vue logo',
+//         src: img
+//       }),
+//       createVNode(HelloWorld, {
+//         msg: 'Welcome to Your Vue.js + TypeScript App',
+//         age: 18
+//       })
+//     ])
+//   }
+// })
 
 createApp(App).mount('#app')
