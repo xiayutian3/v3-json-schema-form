af159aa3d175a4e56cf66eb932ed96eea9d88dc2
1127071993@qq.com <aini2009626>
2022-02-16 21:44:04 +0800

    6-2 如何部署jest单元测试

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,37 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent, h } from 'vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+const HelloWorld = defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup: (props) => {
+    return () => {
+      return h('div', props.msg)
+    }
+  }
+})
 
+// 测试套件
 describe('HelloWorld.vue', () => {
+  // 测试用例
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       props: { msg }
     })
+    // 断言
     expect(wrapper.text()).toMatch(msg)
+    // // 断言    不符合的情况
+    // expect(wrapper.text()).not.toMatch(msg)
   })
+})
+// 测试用例（这种情况用的不是很多）
+test('renders props.msg when passed', () => {
+  const msg = 'new message'
+  const wrapper = shallowMount(HelloWorld, {
+    props: { msg }
+  })
+  expect(wrapper.text()).toMatch(msg)
 })
