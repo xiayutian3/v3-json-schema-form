09baf2f74b853520a2227627b5dcbfd6f0e298df
1127071993@qq.com <aini2009626>
2022-02-18 22:34:21 +0800

    6-6 正式开始单元测试补全

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -3,7 +3,7 @@ import { defineComponent, h } from 'vue'
 import JsonSchemaForm, { NumberField } from '../../lib'
 
 describe('JsonSchemaForm', () => {
-  it('should render correct number field', () => {
+  it('should render correct number field', async () => {
     let value = ''
     const wrapper = mount(JsonSchemaForm, {
       props: {
@@ -18,5 +18,14 @@ describe('JsonSchemaForm', () => {
     const numberFiled = wrapper.findComponent(NumberField)
     // 断言 NumberField 确定会被渲染
     expect(numberFiled.exists()).toBeTruthy()
+    // // 调用onChange 传‘123’，（假设是第三方写的组件，我们不关心它的实现，只需要验证它的方法对不对）
+    // await numberFiled.props('onChange')('123')
+    // expect(value).toBe('123')
+    // 如果是我们自己写的组件，我们要考虑它的一个实现过程，怎么触发，value变化
+    const input = numberFiled.find('input')
+    input.element.value = '123'
+    // 手动触发input事件
+    input.trigger('input')
+    expect(value).toBe(123)
   })
 })
