b96ef590c50d1e203679152f573d05c5fe197936
1127071993@qq.com <aini2009626>
2022-03-16 22:21:49 +0800

    add:github actions workflows

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,9 @@ const CircularDependencyPlugin = require('circular-dependency-plugin')
 // 环境变量 type，这个是我们自己加上去的 在package 命令那里
 const isLib = process.env.TYPE === 'lib'
 
+// git actions 配置 CI 自动化构建的时候添加的配置   .yml
+// const isCI = process.env.CI
+
 module.exports = {
   chainWebpack (config) {
     // 库的模式下不需要 monaco 这个编辑器，只是demo演示的时候用而已
